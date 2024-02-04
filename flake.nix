561e11fcb46238d68d218b56ed7aa004db0d0707
Sam Lehman <github@samlehman.dev>
2024-02-04 03:33:30 -0500

    flake: init inputs.flake-parts [WIP]

--- flake.nix
+++ flake.nix
@@ -1,33 +1,72 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:Hercules-CI/flake-parts";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      systems = [
-        "x86_64-linux"
-        "i686-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "armv6l-linux"
-        "armv7l-linux"
-      ];
-      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
-    in
-    {
-      legacyPackages = forAllSystems (system: import ./default.nix {
-        pkgs = import nixpkgs { inherit system; };
-      });
-      packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
+  outputs = {self, ...} @ inputs: let
+    systems = [
+      "x86_64-linux"
+      "i686-linux"
+      "x86_64-darwin"
+      "aarch64-linux"
+      "armv6l-linux"
+      "armv7l-linux"
+    ];
+    forAllSystems = f: inputs.nixpkgs.lib.genAttrs systems (system: f system);
+  in
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [];
+      inherit systems;
+      #perInput = {};
+      perSystem = {
+        config,
+        lib,
+        pkgs,
+        inputs',
+        self',
+        system, # getSystem,
+        #moduleWithSystem,
+        #withSystem,
+        ...
+      }: {
+        #apps = {};
+        #checks = {};
+        #debug = false;
+        #devShells = {};
+        #formatter = null;
+        #legacyPackages = {};
+        #overlayAttrs = {};
+        #packages = {};
+      };
+      flake = {
+        #apps = {};
+        #checks = {};
+        #devShells = {};
+        #flakeModule = self.flakeModules.default;
+        #flakeModules = {};
+        #formatter = {};
+        legacyPackages = forAllSystems (system:
+          import ./default.nix {
+            pkgs = import inputs.nixpkgs {inherit system;};
+          });
+        #nixosConfiguratoins = {};
+        #nixosModules = {};
+        #overlays = {};
+        packages = forAllSystems (system:
+          inputs.nixpkgs.lib.filterAttrs
+          (_: v: inputs.nixpkgs.lib.isDerivation v)
+          self.legacyPackages.${system});
+      };
     };
-
   description = "Lehmanator's NUR repository";
   nixConfig = {
     accept-flake-config = true;
     experimental-features = ["nix-command" "flakes" "repl-flake"];
     fallback = true;
     extra-substituters = ["https://lehmanator.cachix.org/"];
-    extra-trusted-public-keys = ["lehmanator.cachix.org-1:kT+TO3tnSoz+lxk2YZSsMOtVRZ7Gc57jaKWL57ox1wU="];
+    extra-trusted-public-keys = [
+      "lehmanator.cachix.org-1:kT+TO3tnSoz+lxk2YZSsMOtVRZ7Gc57jaKWL57ox1wU="
+    ];
   };
 }
